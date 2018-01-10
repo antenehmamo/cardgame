0a2b680cc0954765a1804c7845de93a6227d830c
antenehmamo <anteneh.gebreselassie@iongroup.com>
2018-01-10 18:55:25 -0500

    first commit

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+
+import React, {PropTypes} from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div className="Cardcontainer">
+        Card component ...
+     </div>);
+  }
+};
